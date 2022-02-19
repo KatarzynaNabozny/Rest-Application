94e8737b7a819ff38738fc91c3578860e426951f
Katarzyna Nabożny <knnabozny@gmail.com>
2022-02-19 22:44:10 +0100

    Changes in controller

--- src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
+++ src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
@@ -3,9 +3,10 @@ package com.crud.tasks.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+@CrossOrigin("*")
 @ControllerAdvice
 class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
 
--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
 import java.util.List;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
