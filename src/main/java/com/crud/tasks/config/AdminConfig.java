ddcc919f947a70416921eefd821755e24a63718c
Katarzyna Nabożny <knnabozny@gmail.com>
2022-05-18 19:27:00 +0200

    32.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,17 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.mail}")
+    private String companyMail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
 }
